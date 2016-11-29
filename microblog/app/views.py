8c0941108d367a00a89666abe1a29eb594de6255
Kaelin Hanson <kaelinhanson@Kaelins-MacBook-Air.local>
2016-11-29 11:48:53 -0600

    commit

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -63,7 +63,7 @@ def login():
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
-        flash('Invalid Login. Please try again.')
+        flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
     user = User.query.filter_by(email=resp.email).first()
     if user is None:
@@ -74,12 +74,16 @@ def after_login(resp):
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
+        # make the user follow him/herself
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
+    login_user(user, remember=remember_me)
     return redirect(request.args.get('next') or url_for('index'))
+
 
 @app.route('/logout')
 def logout():
